5013fa6cb422797515deebc348bc1a16156a872e
Joker <sunxu.me@qq.com>
2021-06-19 17:17:52 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '604b12e5c23e4efe9a744b8b7518fbc0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'jokerSun',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'jokerSun',
 
   // open graph metadata (optional)
   description: 'Example site description',
